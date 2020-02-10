eae8ef700f2f5a7e8027d3352e17fa3371190a3d
torystosse <tory.mae.stosse@gmail.com>
2020-02-10 14:32:38 -0500

    Fixed heroku URL
    Removed extra `/` at the end of the heroku URL in config.js

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://sleepy-lake-90376.herokuapp.com/',
+  production: 'https://sleepy-lake-90376.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
